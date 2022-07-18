48058bc599d37f9da46a457d828edd86346a3a9a
DarkBear0121 <darkbear0112@gmail.com>
2022-07-19 00:15:03 +0900

    chore: add hardhat config library

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.13",
