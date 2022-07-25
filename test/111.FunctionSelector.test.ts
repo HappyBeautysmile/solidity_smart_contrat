7b1afbee67abc8635652eb0919daf987ff9fdca7
DarkBear0121 <darkbear0112@gmail.com>
2022-07-25 09:22:16 +0900

    test: Create FunctionSelector test code

---
+++ test/111.FunctionSelector.test.ts
@@ -0,0 +1,52 @@
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { ZERO_ADDRESS } from "../helpers/constant";
+import { converter } from "../helpers/unit-converter";
+
+describe("111.FunctionSelector", () => {
+  let functionSelector: Contract;
+
+  let deployer: SignerWithAddress;
+
+  before(async () => {
+    [deployer] = await ethers.getSigners();
+
+    const FunctionSelector = await ethers.getContractFactory(
+      "FunctionSelector"
+    );
+    functionSelector = await FunctionSelector.deploy();
+  });
+
+  describe("FunctionSelector", async () => {
+    it(`Function : getSelector : Success : "transfer(address,uint256)"`, async () => {
+      const transfer = "transfer(address,uint256)";
+      const transferBytes4 = "0xa9059cbb";
+
+      const getSelector = await functionSelector.getSelector(transfer);
+      expect(getSelector).to.equal(transferBytes4);
+    });
+
+    it(`Function : getSelector : Success : "transferFrom(address,address,uint256)"`, async () => {
+      const transferFrom = "transferFrom(address,address,uint256)";
+      const transferFromBytes4 = "0x23b872dd";
+
+      const getSelector = await functionSelector.getSelector(transferFrom);
+      expect(getSelector).to.equal(transferFromBytes4);
+    });
+
+    it(`Function : getSelector : Failed : "transfer(address, uint256)" : Spacing is not available`, async () => {
+      const transferWithSpacing = "transfer(address, uint256)";
+      const transferBytes4 = "0xa9059cbb";
+
+      const getSelector = await functionSelector.getSelector(
+        transferWithSpacing
+      );
+      expect(getSelector).to.not.equal(transferBytes4);
+    });
+  });
+});
