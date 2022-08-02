6c62f8c91af8d3d65d0bba5caa536ac6df753b50
DarkBear0121 <darkbear0112@gmail.com>
2022-08-02 23:56:19 +0900

    feat: Install web3 plugin for test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-web3";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.13",
