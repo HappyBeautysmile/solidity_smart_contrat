7e1d43cf356b1fbe4efee5fcf026f70f500fc119
DarkBear0121 <darkbear0112@gmail.com>
2022-08-03 17:19:11 +0900

    test: Complete Immutable test code

---
+++ test/006.Immutable.test.ts
@@ -0,0 +1,30 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("006.Immutable", () => {
+  let immutable: Contract;
+  let deployer: SignerWithAddress;
+
+  before(async () => {
+    [deployer] = await ethers.getSigners();
+  });
+
+  describe("Immutable", async () => {
+    it(`Function : constructor : need parameters to set immutable variables`, async () => {
+      const Immutable = await ethers.getContractFactory("Immutable");
+      immutable = await Immutable.deploy(123);
+      await immutable.deployed();
+    });
+
+    it(`Verification : variables`, async () => {
+      const MY_ADDRESS = await immutable.MY_ADDRESS();
+      expect(MY_ADDRESS).to.equal(deployer.address);
+
+      const MY_UINT = await immutable.MY_UINT();
+      expect(MY_UINT).to.equal(123);
+    });
+  });
+});
