54bb5009619ab393c5617a8c876dac72ffd7aca4
DarkBear0121 <darkbear0112@gmail.com>
2022-08-05 09:18:29 +0900

    test: Complete ForAndWhileLoop test code

---
+++ test/011.ForAndWhileLoop.ts
@@ -0,0 +1,30 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("011.ForAndWhileLoop", () => {
+  let loop: Contract;
+  let deployer: SignerWithAddress;
+
+  before(async () => {
+    [deployer] = await ethers.getSigners();
+
+    const Loop = await ethers.getContractFactory("Loop");
+    loop = await Loop.deploy();
+    await loop.deployed();
+  });
+
+  describe("Loop", async () => {
+    it(`Function : loop : Success`, async () => {
+      const loopResult = await loop.loop();
+      expect(loopResult).to.equal(5);
+    });
+
+    it(`Function : whileLoop : Success`, async () => {
+      const whileLoop = await loop.whileLoop();
+      expect(whileLoop).to.equal(10);
+    });
+  });
+});
