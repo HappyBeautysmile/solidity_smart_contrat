0581e4e454c32388ba6ca9ce7a0576082af87934
DarkBear0121 <darkbear0112@gmail.com>
2022-08-08 22:00:37 +0900

    mod: Modify Array contract

--- contracts/Array.sol
+++ contracts/Array.sol
@@ -63,7 +63,7 @@ contract ArrayRemoveByShifting {
     // [1, 2, 3, 4, 5, 6] -- remove(2) --> [1, 2, 4, 5, 6, 6] --> [1, 2, 4, 5, 6]
     // [1, 2, 3, 4, 5, 6] -- remove(0) --> [2, 3, 4, 5, 6, 6] --> [2, 3, 4, 5, 6]
     // [1] -- remove(0) --> [1] --> []
-    uint256[] public arr;
+    uint256[] public arr = [1, 2, 3, 4, 5];
 
     function remove(uint256 _index) public {
         require(_index < arr.length, "index out of bound");
@@ -75,21 +75,8 @@ contract ArrayRemoveByShifting {
         arr.pop();
     }
 
-    function test() external {
-        arr = [1, 2, 3, 4, 5];
-        remove(2);
-        //[1, 2, 4, 5]
-
-        assert(arr[0] == 1);
-        assert(arr[1] == 2);
-        assert(arr[2] == 4);
-        assert(arr[3] == 5);
-        assert(arr.length == 4);
-
-        arr = [1];
-        remove(0);
-        // []
-        assert(arr.length == 0);
+    function getArr() public view returns (uint256[] memory) {
+        return arr;
     }
 }
 
@@ -97,7 +84,7 @@ contract ArrayRemoveByShifting {
 Remove array element by copying last element into the place to remove 
 */
 contract ArrayReplaceFromEnd {
-    uint256[] public arr;
+    uint256[] public arr = [1, 2, 3, 4, 5];
 
     // Deleting an element creates a gap in the array.
     // One trick to keep the array compact is to
@@ -110,20 +97,7 @@ contract ArrayReplaceFromEnd {
         arr.pop();
     }
 
-    function test() public {
-        arr = [1, 2, 3, 4];
-
-        remove(1);
-        // [1, 4, 3]
-        assert(arr.length == 3);
-        assert(arr[0] == 1);
-        assert(arr[1] == 4);
-        assert(arr[2] == 3);
-
-        remove(2);
-        // [1, 4]
-        assert(arr.length == 2);
-        assert(arr[0] == 1);
-        assert(arr[1] == 4);
+    function getArr() public view returns (uint256[] memory) {
+        return arr;
     }
 }
