2c102d4944d60e7275f59c129a4fd65ef2eb0f35
DarkBear0121 <darkbear0112@gmail.com>
2022-08-10 12:04:17 +0900

    feat: Create Function contract

---
+++ contracts/Function.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+There are several ways to return outputs from a function.
+Public functions cannot accpet certain data types as inputs or outputs.
+*/
+
+contract Function {
+    // Functions can return multiple values.
+    function returnMany()
+        public
+        pure
+        returns (
+            uint256,
+            bool,
+            uint256
+        )
+    {
+        return (1, true, 2);
+    }
+
+    // Return values can be named.
+    function named()
+        public
+        pure
+        returns (
+            uint256 x,
+            bool b,
+            uint256 y
+        )
+    {
+        return (1, true, 2);
+    }
+
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned()
+        public
+        pure
+        returns (
+            uint256 x,
+            bool b,
+            uint256 y
+        )
+    {
+        x = 1;
+        b = true;
+        y = 2;
+    }
+
+    // Use destructuring assignment when calling another
+    // function that returns multiple values.
+    function destructuringAssignments()
+        public
+        pure
+        returns (
+            uint256,
+            bool,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        (uint256 i, bool b, uint256 j) = returnMany();
+
+        // Values can be left out.
+        (uint256 x, , uint256 y) = (4, 5, 6);
+
+        return (i, b, j, x, y);
+    }
+
+    // Cannot use map for either input or output
+
+    // Can use array for input
+    function arrayInput(uint256[] memory _arr)
+        public
+        pure
+        returns (uint256[] memory)
+    {
+        return _arr;
+    }
+
+    // Can use array for output
+    uint256[] public arr;
+
+    function arrayOutput() public view returns (uint256[] memory) {
+        return arr;
+    }
+}
