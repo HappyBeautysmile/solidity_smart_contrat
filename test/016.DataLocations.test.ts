0cf3fa0c600b95e31c5b3221bcb3b4c8855980b5
DarkBear0121 <darkbear0112@gmail.com>
2022-08-10 11:44:08 +0900

    test: Complete DataLocations test code

---
+++ test/016.DataLocations.test.ts
@@ -0,0 +1,31 @@
+import { PANIC_CODES } from "@nomicfoundation/hardhat-chai-matchers/panic";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("016.DataLocations", () => {
+  let dataLocations: Contract;
+
+  before(async () => {
+    const DataLocations = await ethers.getContractFactory("DataLocations");
+    dataLocations = await DataLocations.deploy();
+    await dataLocations.deployed();
+  });
+
+  describe("DataLocations", () => {
+    it("Function : f : Success", async () => {
+      const fTx = await dataLocations.f();
+      await fTx.wait();
+    });
+
+    it("Function : g : Success", async () => {
+      const gTx = await dataLocations.g([]);
+      await gTx.wait();
+    });
+
+    it("Function : h : Success", async () => {
+      const hTx = await dataLocations.h([]);
+      await hTx.wait();
+    });
+  });
+});
