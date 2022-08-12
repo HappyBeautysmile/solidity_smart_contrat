9a9ae82b878fc74377020e7b4424fea74eb81fbf
DarkBear0121 <darkbear0112@gmail.com>
2022-08-12 19:44:41 +0900

    feat: Create Error contract

---
+++ contracts/Error.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+Error
+
+An error will undo all changes made to the state during a transaction.
+You can throw an error by calling require, revert or assert.
+  - require is used to validate inputs and conditions before execution.
+  - revert is similar to require. See the code below for details.
+  - assert is used to check for code that should never be false.
+    Failing assertion probably means that there is a bugs.
+*/
+contract Error {
+    function testRequire(uint256 _i) public pure {
+        // Require should be used to validate conditions such as:
+        //  - inputs
+        //  - conditions before execution
+        //  - return values from calls to other functions
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint256 _i) public pure {
+        // Revert is useful when the condition to check is complex.
+        // This code does the exact same thing as the example above
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint256 public num;
+
+    function testAssert() public view {
+        // Assert should only be used to test for internal errors,
+        // and to check invariants.
+
+        // Here we assert that number is always equal to 0
+        // since it is impossible to update the value of num
+        assert(num == 0);
+    }
+
+    function setNum(uint256 _num) external {
+        num = _num;
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+    }
+}
